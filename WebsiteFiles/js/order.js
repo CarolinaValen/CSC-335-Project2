03f08a00c693626d1c575a58dabae1fcf16ac7d8
CarolinaValen <87994550+CarolinaValen@users.noreply.github.com>
2022-05-23 04:57:34 -0400

    Update order.js

--- WebsiteFiles/js/order.js
+++ WebsiteFiles/js/order.js
@@ -1,24 +1,18 @@
 let saveFile = () => {
-    	
-    // Get the data from each element on the form.
+   
     const name = document.getElementById('Name');
     const email = document.getElementById('Email');
     const tea = document.getElementById('Tea');
     const quantity = document.getElementById('Quantity');
     
-    
-    // This variable stores all the data.
     let data = 
         '\r Name: ' + name.value + ' \r\n ' + 
         'Email: ' + email.value + ' \r\n ' + 
         'Tea: ' + tea.value + ' \r\n ' + 
         'Quantity: ' + quantity.value;
        
-    
-    // Convert the text to BLOB.
     const textToBLOB = new Blob([data], { type: 'text/plain' });
     const sFileName = 'Order.txt';	   // The file to save the data.
-
     let newLink = document.createElement("a");
     newLink.download = sFileName;
 
