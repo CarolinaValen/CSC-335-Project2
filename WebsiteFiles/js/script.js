df8bc266470a32c13ee5dd29e7cc6eea9f1c500a
CarolinaValen <87994550+CarolinaValen@users.noreply.github.com>
2022-05-23 00:35:12 -0400

    Add files via upload

---
+++ WebsiteFiles/js/script.js
@@ -0,0 +1,45 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'Welcome to Peace '
+let idx = 1
+let speed = 300 / speedEl.value
+const buttons = document.querySelectorAll('.ripple')
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
